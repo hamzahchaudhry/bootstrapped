c192b83e4b5f66d7bd79bd52b2b478b38afd4b56
hamzah <hamzah.chaudhry@proton.me>
2025-12-06 22:46:03 -0800

    initial commit

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+i686-elf-as src/boot.s -o boot.o
+i686-elf-gcc -c src/kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
