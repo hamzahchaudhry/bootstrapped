559b56b59d03f25dbc6de5edcd77449311035cc6
hamzah <hamzah.chaudhry@proton.me>
2025-12-08 19:00:46 -0800

    simple polling keyboard support

---
+++ kernel/include/kernel/keyboard.h
@@ -0,0 +1,6 @@
+#ifndef _KERNEL_KEYBOARD_H
+#define _KERNEL_KEYBOARD_H
+
+unsigned char keyboard_poll(void);
+
+#endif
--- libc/stdio/putchar.c
+++ libc/stdio/putchar.c
@@ -8,7 +8,7 @@ int putchar(int c)
 {
 #if defined(__is_libk)
   unsigned char uc = (unsigned char)c;
-  terminal_write(&uc, sizeof(uc));
+  terminal_write((const char *)&uc, sizeof(uc));
 #else
   // TODO: Implement stdio and the write system call.
 #endif
