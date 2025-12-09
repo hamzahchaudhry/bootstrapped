e265dd645ac11b26ee89703519e6610eebfeb845
hamzah <hamzah.chaudhry@proton.me>
2025-12-08 19:00:46 -0800

    simple polling keyboard support

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+#include <kernel/keyboard.h>
 #include <kernel/tty.h>
 
 /* kernel entry point, called from _start in boot.s. */
@@ -7,5 +8,14 @@ void kernel_main(void)
 {
   /* initialize terminal interface */
   terminal_initialize();
-  printf("You're in bootstrapped!");
+  printf("You're in bootstrapped!\n");
+  printf("Type keys to see scancode->ASCII polling:\n");
+
+  /* simple busy-wait poll */
+  // TODO: replace with IRQ system.
+  while(1)
+    {
+      unsigned char c = keyboard_poll();
+      if(c) putchar(c);
+    }
 }
