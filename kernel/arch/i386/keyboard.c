0abe50fecb8b1d765530300bef6f17edf92e6e0a
hamzah <hamzah.chaudhry@proton.me>
2026-01-19 02:10:12 -0800

    add shift key and caps lock keyboard support

--- kernel/arch/i386/keyboard.c
+++ kernel/arch/i386/keyboard.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdint.h>
 
 #include <kernel/keyboard.h>
@@ -13,7 +14,10 @@ static volatile uint8_t kbd_buf[KBD_BUF_SIZE];
 static volatile uint8_t kbd_head;
 static volatile uint8_t kbd_tail;
 
-/* set-1 scancode -> ASCII for unmodified keys. zero for non-printables. */
+static volatile bool shift_down;
+static volatile bool caps_lock;
+
+/* set-1 scancode -> ASCII for unmodified keys */
 static const unsigned char scancode_ascii[128]
   = {[0x02] = '1', [0x03] = '2',  [0x04] = '3',  [0x05] = '4',  [0x06] = '5',
      [0x07] = '6', [0x08] = '7',  [0x09] = '8',  [0x0A] = '9',  [0x0B] = '0',
@@ -26,6 +30,21 @@ static const unsigned char scancode_ascii[128]
      [0x2C] = 'z', [0x2D] = 'x',  [0x2E] = 'c',  [0x2F] = 'v',  [0x30] = 'b',
      [0x31] = 'n', [0x32] = 'm',  [0x33] = ',',  [0x34] = '.',  [0x35] = '/',
      [0x39] = ' '};
+
+/* set-1 scancode -> ASCII for shifted keys */
+static const unsigned char shifted_scancode_ascii[128]
+  = {[0x02] = '!', [0x03] = '@',  [0x04] = '#',  [0x05] = '$',  [0x06] = '%',
+     [0x07] = '^', [0x08] = '&',  [0x09] = '*',  [0x0A] = '(',  [0x0B] = ')',
+     [0x0C] = '_', [0x0D] = '+',  [0x0E] = '\b', [0x0F] = '\t', [0x10] = 'Q',
+     [0x11] = 'W', [0x12] = 'E',  [0x13] = 'R',  [0x14] = 'T',  [0x15] = 'Y',
+     [0x16] = 'U', [0x17] = 'I',  [0x18] = 'O',  [0x19] = 'P',  [0x1A] = '{',
+     [0x1B] = '}', [0x1C] = '\n', [0x1E] = 'A',  [0x1F] = 'S',  [0x20] = 'D',
+     [0x21] = 'F', [0x22] = 'G',  [0x23] = 'H',  [0x24] = 'J',  [0x25] = 'K',
+     [0x26] = 'L', [0x27] = ':',  [0x28] = '"',  [0x29] = '~',  [0x2B] = '|',
+     [0x2C] = 'Z', [0x2D] = 'X',  [0x2E] = 'C',  [0x2F] = 'V',  [0x30] = 'B',
+     [0x31] = 'N', [0x32] = 'M',  [0x33] = '<',  [0x34] = '>',  [0x35] = '?',
+     [0x39] = ' '};
+
 
 static void kbd_put(uint8_t c)
 {
@@ -47,10 +66,21 @@ void keyboard_irq_handler(void)
 {
   uint8_t scancode = inb(DATA_PORT);
 
-  /* ignore break codes (release) and extended prefixes for now. */
+  if (scancode == 0x2A || scancode == 0x36) shift_down = true;
+  if (scancode == 0xAA || scancode == 0xB6) shift_down = false;
+  if (scancode == 0x3A) caps_lock = !caps_lock;
+
   if ((scancode & 0x80) == 0 && scancode < sizeof(scancode_ascii))
   {
-    unsigned char c = scancode_ascii[scancode];
+    unsigned char c = shift_down ? shifted_scancode_ascii[scancode] : scancode_ascii[scancode];
+    if (c >= 'A' && c <= 'Z')
+    {
+      if (!(shift_down ^ caps_lock)) c = (unsigned char)(c + 32);
+    }
+    else if (c >= 'a' && c <= 'z')
+    {
+      if (shift_down ^ caps_lock) c = (unsigned char)(c - 32);
+    }
     if (c) kbd_put((uint8_t)c);
   }
 
